33fbb0f29adf5051ca72659268d03a8f73ee7929
John Huang <johnhuang121@gmail.com>
2025-04-11 13:05:40 -0700

    changes admin panel for customized order and filters

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
+
+# class ChoiceInline(admin.TabularInline):
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ["pub_date", "question_text"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]})
+    ]
+    inlines = [ChoiceInline]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
